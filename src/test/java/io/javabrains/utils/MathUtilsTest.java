8a351e72f31854987f4e0fa7a7a3730ae1317f34
Aeshan rosa <aeshaneruditharosa@gmail.com>
2025-11-05 10:18:34 +0530

    2025-11-05 Junit workshop

--- src/test/java/io/javabrains/utils/MathUtilsTest.java
+++ src/test/java/io/javabrains/utils/MathUtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+//learned on 2025-11-05
 class MathUtilsTest {
 	
 	private MathUtils mathUtils;
@@ -57,6 +58,29 @@ class MathUtilsTest {
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), 
 				"Divide should throw ArithmeticException when denominator is zero");
 	}
-	
+
+    //mine
+    @Test
+    void testSubstract(){
+        assertEquals(3,mathUtils.subtract(4,1),"subtact should shoe the difference.");
+    }
+
+    @Nested
+    class SubTwo{
+        @Test
+        void testSubtractTwoPositive(){
+            assertEquals(4,mathUtils.subtract(5,1),"Testing two positive numbers.");
+        }
+
+        @Test
+        void testSubtractTwoNegetive(){
+            assertEquals(-5,mathUtils.subtract(-10,-5),"Testing two Negetive numbers");
+        }
+
+        @Test
+        void testSubtractPositiveNegetive(){
+            assertEquals(8,mathUtils.subtract(5,-3),"Testing one positive and one negetive");
+        }
+    }
 	
 }
